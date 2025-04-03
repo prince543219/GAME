305ef5d88371e17fe43f45ed65a2bf24b56d2e75
prince543219 <surajitsingh54321@gmail.com>
2025-04-03 08:01:26 -0700

    Update score.py

--- commands/score.py
+++ commands/score.py
@@ -1,8 +1,15 @@
+#from telethon import events
+
+#from games.word_scramble import stop_game
+
+#command = "score12"
+
+#async def handler(event):
+    #await event.reply("woking on it !will be added soon.")
 from telethon import events
 
-from games.word_scramble import stop_game
-
-command = "score12"
+from games.word_scramble import display_final_scores
+command = "leaderboard"
 
 async def handler(event):
-    await event.reply("woking on it !will be added soon.")
+    await event.reply("leaderboard :-\n")
