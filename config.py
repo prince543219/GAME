0813f53f6bc7dfa50e0bfe4112c6334852199392
prince543219 <surajitsingh54321@gmail.com>
2025-04-11 08:59:16 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = '29565251'  # You can get this from https://my.telegram.org
-API_HASH = 'afaa92769fc8a8f85dbf1a11c2b41958'  # You can get this from https://my.telegram.org
-BOT_TOKEN = '7121041164:AAFzbscZzEGTgHliUdktGk9claHR7c9N3xE'  # You get this from @BotFather
+API_ID = '   '  # You can get this from https://my.telegram.org
+API_HASH = '      '  # You can get this from https://my.telegram.org
+BOT_TOKEN = '     '  # You get this from @BotFather
